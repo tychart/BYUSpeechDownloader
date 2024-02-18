3f423c6522ad00e3078c31fe047643630ed2b799
tychart <tyler@thechartrands.com>
2024-02-18 15:24:01 -0700

    Adding Customkinter GUI

---
+++ test02.py
@@ -0,0 +1,24 @@
+import customtkinter as ctk
+
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("my app")
+        self.geometry("400x180")
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure((0, 1), weight=1)
+
+        self.checkbox_1 = ctk.CTkCheckBox(self, text="checkbox 1")
+        self.checkbox_1.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="w")
+        self.checkbox_2 = ctk.CTkCheckBox(self, text="checkbox 2")
+        self.checkbox_2.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="w")
+        self.button = ctk.CTkButton(self, text="my button", command=self.button_callback)
+        self.button.grid(row=3, column=0, padx=10, pady=10, sticky="ew")
+
+    def button_callback(self):
+        print("button pressed")
+
+app = App()
+app.mainloop()
+
